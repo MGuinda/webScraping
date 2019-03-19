fa5018ca1df96b7c1520ff5da50375033db782bf
MGuinda <48735248+MGuinda@users.noreply.github.com>
2019-03-19 22:32:48 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+#Creamos una instancia de Flask y le damos la instancia a API
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Hello(Resource):
+    def get(self, name):
+        return {"Hello":name}
+
+api.add_resource(Hello, '/hello/<name>')
+
+if __name__ == '__main__':
+ app.run(debug=True)
